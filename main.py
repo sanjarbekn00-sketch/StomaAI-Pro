e917d02ab8ec9c2726656eaa97ab5eb532c9ee1c
sanjarbekn00-sketch <sanjarbekn00@gmail.com>
2026-04-08 03:48:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import io
 import os
 
 # --- 1. КОНФИГУРАЦИЯ API ---
-API_KEY = "AIzaSyCXMFQikm6XwydOXJfPQJrItJESnwdbBlI" 
+API_KEY = "AIzaSyCNo5cmQl1nvnqirqTJAvRTzma_9VcSSDE" 
 genai.configure(api_key=API_KEY)
 llm = genai.GenerativeModel('gemini-2.5-flash')
 
